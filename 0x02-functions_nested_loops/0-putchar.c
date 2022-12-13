57242286e52ee709337095e66a51650ea340d3c2
Achiugo Trust <achiugotrust@gmail.com>
2022-12-13 10:47:29 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * main - print "_putchar" followed by newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char array[8] = "_putchar";
+	int index;
+
+	while (index < 8)
+	{
+		_putchar(array[index]);
+		index++;
+	}
+	_putchar(9);
+	return (0);
+}
