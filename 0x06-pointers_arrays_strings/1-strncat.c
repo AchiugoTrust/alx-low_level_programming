cf7560d7f62a23f51478eb5e358f1b05a4c36cd2
Achiugo Trust <achiugotrust@gmail.com>
2022-12-21 14:45:22 +0100

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+/**
+ * _strncat - concatenates strings up to n
+ * @src: input source string to append to dest
+ *
+ * @dest: input destination string to append to
+ *
+ * @n: input number of bytes to append
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
