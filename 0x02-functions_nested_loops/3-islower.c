ac5c7f045dba6b1fa3e914658bd948d0ebf6e177
Achiugo Trust <achiugotrust@gmail.com>
2022-12-23 12:50:17 +0100

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+
+/**
+ * _islower - check for lowercase character
+ * @c: character to check
+ *
+ * Return: 1 if lowercase; 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
