240a865bb98fa66b91efbf79575a071dd9e51191
Achiugo Trust <achiugotrust@gmail.com>
2023-01-04 05:01:26 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - fills memory with constant byte
+ * Return - pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	int num = +n;
+
+	for (i = 0; i < num; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
