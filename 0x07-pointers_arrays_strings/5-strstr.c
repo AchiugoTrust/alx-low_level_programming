bed264155a1009452b82dc561cb8eaa61ee5057e
Achiugo Trust <achiugotrust@gmail.com>
2023-01-04 05:55:22 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+/**
+ * _strstr - locates a substring
+ * Return - pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int size = 0;
+	int x = 0;
+
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+		size++;
+	}
+	if (size == 0)
+	{
+		return (haystack);
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			x = 0;
+			while (x < size && haystack[i + x] == needle[x])
+			{
+				if (x == (size - 1))
+				{
+					return (&haystack[i]);
+				}
+				x++;
+			}
+		}
+	}
+	if (haystack[i] == '\0' && needle[x] == '\0')
+	{
+		return (&haystack[i]);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
