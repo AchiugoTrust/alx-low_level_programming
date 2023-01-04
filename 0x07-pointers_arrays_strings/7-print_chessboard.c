d80fa844aa7c707c4b3e078f0afe937ce2fbebdd
Achiugo Trust <achiugotrust@gmail.com>
2023-01-04 06:00:36 +0100

    print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+/**
+ * chessboard - function prints chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int rows = 0;
+	int columns = 0;
+
+	for (rows = 0; rows < 8; rows++)
+	{
+		for (columns = 0; columns < 8; columns++)
+		{
+			_putchar(a[rows][columns]);
+		}
+		_putchar(10);
+	}
+}
