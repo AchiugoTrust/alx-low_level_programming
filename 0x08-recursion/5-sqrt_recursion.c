aa6c08dcfc36d43e06ff2fca4fd0521198e51ec6
Achiugo Trust <achiugotrust@gmail.com>
2023-01-06 00:11:23 +0100

    sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,6 +5,27 @@
  * sqrt_recursion - returns the natural square root of a number
  * return - always 0
  */
+
+int square_root_recursion(int n, int trialNum)
+{       /* trialNum - potential square root number */
+
+        int trial;
+        trial = ((n / trialNum) + 1 + trialNum) / 2;
+
+        if ((n / trialNum) == trialNum && n % trialNum == 0)
+        {
+                return (trialNum);
+        }
+        else if (trial == trialNum)
+        {
+                return (-1);
+        }
+        else
+        {
+                return (square_root_recursion(n, trial));
+        }
+}
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -16,24 +37,3 @@ int _sqrt_recursion(int n)
 		return (square_root_recursion(n, (n / 2) + 1));
 	}
 }
-
-int square_root_recursion(int n, int trialNum)
-{	/* trialNum - potential square root number */
-
-	int trial;
-	trial = ((n / trialNum) + 1 + trialNum) / 2;
-
-	if ((n / trialNum) == trialNum && n % trialNum == 0)
-	{
-		return (trialNum);
-	}
-	else if (trial == trialNum)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (square_root_recursion(n, trial));
-	}
-}
-
