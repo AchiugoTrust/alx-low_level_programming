5da61378d36c2a6db9bd396ca2d8ffd850ff4cd4
Achiugo Trust <achiugotrust@gmail.com>
2023-01-06 00:06:10 +0100

    is_prime_num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
-#include " main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
  * _is_prime_number - returns 1 if the input integer is a prime number
@@ -15,3 +16,20 @@ int is_prime_number(int n)
 		return (counter(n, 1));
 	}
 }
+
+int counter(int n, int count)
+{
+	if (count == n - 1)
+	{
+		return (1);
+	}
+	else if (n % (n - count) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (counter(n, count + 1));
+	}
+}
+
