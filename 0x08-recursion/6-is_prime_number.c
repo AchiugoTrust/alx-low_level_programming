439b331618c58bafee613a65ba11de5dd9a7b3f1
Achiugo Trust <achiugotrust@gmail.com>
2023-01-06 00:08:25 +0100

    is_prime_num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,17 +5,6 @@
  * _is_prime_number - returns 1 if the input integer is a prime number
  * return - always 0
  */
-int is_prime_number(int n)
-{
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else
-	{
-		return (counter(n, 1));
-	}
-}
 
 int counter(int n, int count)
 {
@@ -33,3 +22,14 @@ int counter(int n, int count)
 	}
 }
 
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (counter(n, 1));
+	}
+}
