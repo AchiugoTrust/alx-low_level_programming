b4322ef3ea02013d9ef8618c3b8507103f4b114f
Achiugo Trust <achiugotrust@gmail.com>
2023-01-05 13:19:56 +0100

    strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen-recursion - returns the length of a string
+ * Return - always 0
+ */
+
+int _strlen_recursion(char *s) 
+{
+	if (s[0] != '\0')
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	else
+	{
+		return (0);
+	}
+}
