53eaa37cc8f7ae4c51e40cf990d9f85252e3eade
Achiugo Trust <achiugotrust@gmail.com>
2023-01-05 12:57:28 +0100

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _puts-recursion - prints a string, followed by a new line
+ * Return - Always 0;
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+		_putchar(10);
+	else
+	{
+		_putchar(s[0]);
+		s++;
+		_puts_recursion(s);
+	}
+}
