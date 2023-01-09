5f2e1f2e117285f3fd108fc26dcbbe0bc147a53c
Achiugo Trust <achiugotrust@gmail.com>
2023-01-09 13:41:48 +0100

    libmy

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -7,7 +7,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-_atoi(har *schar *_strcat(char *c);
+int _atoi(har *schar *_strcat(char *c);
 int dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
