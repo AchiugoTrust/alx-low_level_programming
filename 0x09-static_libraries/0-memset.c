d06fa6900f19d0951275cb3605a23a1995dfb537
Achiugo Trust <achiugotrust@gmail.com>
2023-01-10 22:19:02 +0100

    libmy

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with constant byte
+ * Return - pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	int num = +n;
+
+	for (i = 0; i < num; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
