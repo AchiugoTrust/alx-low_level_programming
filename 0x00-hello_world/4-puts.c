6e1434a821b244d49c02b83295caa04953d1c0ae
Achiugo <achiugotrust@gmail.com>
2023-12-14 14:12:39 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - prints a function
+ * return (0) - returns 0
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
