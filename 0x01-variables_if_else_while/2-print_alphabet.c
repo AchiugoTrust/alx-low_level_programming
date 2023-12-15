fb5af6a96bd04d8beb6bde1814cc61d66402f625
Achiugo <achiugotrust@gmail.com>
2023-12-15 16:21:06 +0100

    Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lower case
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a', lower <= 'z', lower++)
+	{
+		putchar(lower);
+	}
+	putchar(\n);
+	return (0);
+
