13a319f47c03b792be0386723f2df89907d4077b
Achiugo <achiugotrust@gmail.com>
2023-12-15 22:54:37 +0100

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+/**
+ * main- entry
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+
+			putchar(' ');
+putchar('0' + b / 10);
+			putchar('0' + b % 10);
+
+			if (i == 98 && b == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
