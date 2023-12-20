9abddfcbe3e8b08adec6649286ca2c358574f229
Achiugo <achiugotrust@gmail.com>
2023-12-20 13:39:33 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * Return: Always 0
+ * @int: Takes an integer
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n <= 0)
+	{
+		_putchar('n' * '-1');
+		return (n);
+	}
+
+	return (0);
