259803cfad4cc7a9af75cb99cc570a124722e427
Achiugo <achiugotrust@gmail.com>
2023-12-20 10:13:16 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include "main.h"
+
 /**
- * putchar - prints a character
- * @c: character to be returned
+ * main - prints putchar followd by a new line
  * Return: Always 0
  */
-char _putchar(char c)
+
+int main(void)
 {
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
 	putchar('c');
-	return (c);
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
