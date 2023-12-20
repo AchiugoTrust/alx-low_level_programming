b8b4b88100119fc21cb3721765754910e04d5d35
Achiugo <achiugotrust@gmail.com>
2023-12-20 14:12:03 +0100

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,15 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @t : My choice parameter
+ * Return: absolute value
  */
 
 int print_last_digit(int t)
 {
-	putchar(t[-1]);
-	return (t);
-}
+	if (t >= 0) 
+	{
+		return (t);
+	} else
+	{
+		return (-t);
+	}
