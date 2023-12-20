9c5d74082ae3b4b9c953b8fa4f22f5ff6843e03a
Achiugo <achiugotrust@gmail.com>
2023-12-20 10:32:56 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	char lower;
+	char no_of_times;
+
+	for (no_of_times = '0'; no_of_times < '10'; no_of_times++)
+	{
+		for (lower = 'a'; lower <= 'z'; lower++)
+		{
+			_putchar(lower);
+		}
+
+		_putchar('\n');
+	}
