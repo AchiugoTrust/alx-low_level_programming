034b88cf937bddd2b1a38d0e56c3886e1512d759
Achiugo <achiugotrust@gmail.com>
2023-12-20 10:22:18 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all aphabets in lower case
+ */
+
+void print_alphabet(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+
+	putchar('\n');
+}
