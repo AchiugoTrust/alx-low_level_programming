e680cd9572eb84105fb9e62fbfeb29392f87690c
Achiugo <achiugotrust@gmail.com>
2023-12-20 14:00:10 +0100

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @t : My choice parameter
+ */
+
+int print_last_digit(int t)
+{
+	putchar(t[-1]);
+	return (t);
+}
