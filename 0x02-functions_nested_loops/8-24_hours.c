52d002f32c253cc76fc1533972ae4cc6bc6467ed
Achiugo <achiugotrust@gmail.com>
2023-12-20 15:30:32 +0100

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 		{
 			_putchar((hrs / 10) + '0');
 			_putchar((hrs % 10) + '0');
-			-putchar(':');
+			_putchar(':');
 			_putchar((mins % 10) + '0');
 			_putchar((mins % 10) + '0');
 			_putchar('\n');
