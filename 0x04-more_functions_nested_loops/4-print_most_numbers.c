20eba1bec5941b5cc74ba0c48296ef154e0e8c3a
Achiugo <achiugotrust@gmail.com>
2023-12-21 21:41:04 +0100

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 - 9
+ * Return: Always 0;
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		while (i != '2' && i != '4')
+		{
+			_putchar (i);
+		}
+
+	_putchar('\n');
+	}
+}
