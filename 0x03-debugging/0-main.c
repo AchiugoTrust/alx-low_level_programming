ea2629dcc78fa4f5ec4154948006d51bfddfb448
Achiugo <achiugotrust@gmail.com>
2023-12-21 12:06:22 +0100

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,17 +5,12 @@
  * @i: Choice integer
  * Return: Always 0
  */
-
 void positive_or_negative(int i)
 {
-	if (i > 0)
+	i = 0;
+
+       	if (i == 0)
 	{
-		printf("%d is positive\n", i);
-	} else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	} else
-	{
-		printf("%d is negative\n", i);
+		printf("%d is zero", i);	
 	}
 }
