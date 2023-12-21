66e0acf1ab1dd1f206ec2cd8487899d3846c68d3
Achiugo <achiugotrust@gmail.com>
2023-12-21 21:15:00 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	product = a * b;
+	return (product);
+}
