29dd2f9f44fefb9129ce3c0fbadf0ad40eb1bf65
Achiugo <achiugotrust@gmail.com>
2024-01-02 17:56:08 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * reset_to_98 - updates a parameter to 98
+ * Return: Always 0;
+ * @n: parameter
+ */
+
+void reset_to_98(int *n)
+{
+	int n = 402;
+	*n = 98;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * putchar - prints a character
+ * @c: character to be returned
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
