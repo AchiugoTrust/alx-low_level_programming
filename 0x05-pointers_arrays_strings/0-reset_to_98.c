998076705047d1a579add7e01084518b2e68f997
Achiugo <achiugotrust@gmail.com>
2024-01-02 17:59:59 +0100

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,11 @@
 /*
  * reset_to_98 - updates a parameter to 98
  * Return: Always 0;
- * @n: parameter
+ * @n: parameter pointed to
+ * @*n: pointer to integer n
  */
 
 void reset_to_98(int *n)
 {
-	n = 402;
 	*n = 98;
-
-	return (0);
 }
