241dd64a8a7b0b7f9dabbeeb05e6d7a245bc3d02
Achiugo <achiugotrust@gmail.com>
2024-01-09 12:56:31 +0100

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset- fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * _memset- fills memory with a specific value
  * @s: pointer to memory
  * @b: constant byte to reset memory with
  * @n: number of bytes to be changed
