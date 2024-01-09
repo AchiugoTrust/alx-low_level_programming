018335d451ac78ff144fe66d26f8b9f948910f8e
Achiugo <achiugotrust@gmail.com>
2024-01-09 18:40:46 +0100

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+        int a, b;
+	
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+			_putchar(a[a][b]);
+		_putchar('\n');
+	}
+}
