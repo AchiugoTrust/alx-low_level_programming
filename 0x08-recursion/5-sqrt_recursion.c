a63f5c1aaad4c94d259dd8b9f0569d229660b033
Achiugo <achiugotrust@gmail.com>
2024-01-10 22:18:11 +0100

    square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,3 +17,24 @@ int _sqrt_recursion(int n)
 		return (square_root_recursion(n, (n / 2) + 1));
 	}
 }
+
+int square_root_recursion(int n, int guess)
+{
+	int newguess;
+
+	newguess = ((n / guess) + 1 + guess) / 2;
+
+	if ((n / guess) == guess && n % guess == 0)
+	{
+		return (guess);
+	}
+	else if (newguess == guess)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (square_root_recursion(n, newguess));
+	}
+}
+
