cf44aa32f7f76759f001779ca674353415c45bdc
Achiugo <achiugotrust@gmail.com>
2024-01-14 08:04:13 +0100

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -  program that prints the number of arguments with a new line
+ * @argc: program argument counter
+ * @argv: array
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argc - 1);
+	return (0);
+}
