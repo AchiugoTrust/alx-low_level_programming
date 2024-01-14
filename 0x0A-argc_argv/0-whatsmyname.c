fe2cb4b04f76148d9fda7cb5c4f09e1447c30c5f
Achiugo <achiugotrust@gmail.com>
2024-01-14 07:33:05 +0100

    what's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  program that prints its name with a new line
+ * @argc: program argument counter
+ * @argv: array
+ * Return: Always (0)
+ */
+
+int main(int argc, char argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
