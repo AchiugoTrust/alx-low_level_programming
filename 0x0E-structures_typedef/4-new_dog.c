ceeaf3b0bee9bf83dc1fff729d50099e88a6b386
Achiugo <achiugotrust@gmail.com>
2024-01-24 08:15:55 +0100

    trial

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,55 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function to initialize a new dog
+ * @name: dog's name
+ *
+ * @age: dog's age
+ *
+ * @owner: name of dog's owner
+ *
+ * Return: pointer to new data stucture with dog's information
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	char *name_t;
+	char *owner_t;
+	dog_t *doggy = malloc(sizeof(dog_t));
+
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		;
+	name_t = malloc((i + 1) * sizeof(char));
+	if (name_t == NULL)
+	{
+		free(doggy);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		name_t[i] = name[i];
+	name_t[i] = '\0';
+
+	for (i = 0; owner[i]; i++)
+		;
+	owner_t = malloc((i + 1) * sizeof(char));
+	if (owner_t == NULL)
+	{
+		free(doggy);
+		free(name_t);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
+		owner_t[i] = owner[i];
+	owner_t[i] = '\0';
+
+	doggy->name = name_t;
+	doggy->age = age;
+	doggy->owner = owner_t;
+
+	return (doggy);
+}
+
